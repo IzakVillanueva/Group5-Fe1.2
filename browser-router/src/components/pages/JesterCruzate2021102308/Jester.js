c7c05884ec6766a4f86c5543a1abab78914031b2
Pantofu <trolarchefxthpoo@gmail.com>
2024-03-19 10:50:17 +0800

    Added Pages
    Added pages for Jester, Jurich, Ghasutt, Kam, IC within their respective folders and files containing js and css

---
+++ browser-router/src/components/pages/JesterCruzate2021102308/Jester.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './JesterCruzate2021102308/Jester.css';
+
+function Jester() {
+  return (
+    <div className="content">
+      <span>Jester D. Cruzate SN:2021102308</span>
+      
+      </div>
+  )
+}
+
+export default Error
