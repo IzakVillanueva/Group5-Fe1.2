0004647f6e8559443961164cb263635ff2c64b43
Pantofu <trolarchefxthpoo@gmail.com>
2024-03-19 11:01:00 +0800

    Minor Update

---
+++ browser-router/src/components/pages/JoseKam2021101249/Jose.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './JoseKam2021101249/Jose.css';
+
+function Jester() {
+  return (
+    <div className="content">
+      <h2>Jose Roberto Kam SN:2021101249</h2>
+      
+    </div>
+  )
+}
+
+export default Jester
