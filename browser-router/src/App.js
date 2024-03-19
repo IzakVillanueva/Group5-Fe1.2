f12bb2a9021c2a4c296858a463342475933539b8
Izak Villanueva <izakvillanueva14@gmail.com>
2024-03-19 10:25:00 +0800

    Added Footer

--- browser-router/src/App.js
+++ browser-router/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+
+      
+        {/* Importing the Footer component after nested routes */}
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ browser-router/src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./footerstyles.css"
+
+function Footer() {
+  return (
+    <div className="footer">Footer</div>
+  )
+}
+
+export default Footer
