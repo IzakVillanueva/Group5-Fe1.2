54517ddf4f0b72e04087c73f46c21186b360deeb
Izak Villanueva <izakvillanueva14@gmail.com>
2024-03-19 11:00:18 +0800

    Added imports

--- browser-router/src/App.js
+++ browser-router/src/App.js
@@ -5,6 +5,12 @@ import Template from "./components/Template";
 import Nav from "./components/navbar/Nav";
 import Footer from "./components/footer/Footer";
 import Izak from "./components/pages/IzakVillanueva2021103245/Izak";
+import Ghasutt from "./components/pages/GhasuttPatarata2019135835/Ghasutt";
+import Ivanne from "./components/pages/IvanneYabut2021107696/Ivanne";
+import Jester from "./components/pages/JesterCruzate2021102308/Jester";
+import Jose from "./components/pages/JoseKam2021101249/Jose";
+import Jurich from "./components/pages/JurichCeleste2021102215/Jurich";
+
 
 function App() {
   return (
